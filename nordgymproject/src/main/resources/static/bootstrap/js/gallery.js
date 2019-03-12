17ee74261de8929a0f6a2d4df228e896509da64f
Nike <nagrozdanov@gmail.com>
2019-03-12 12:51:42 +0200

    update project

--- nordgymproject/src/main/resources/static/bootstrap/js/gallery.js
+++
@@ -1,5 +0,0 @@
-$(function() {                       //run when the DOM is ready
-    $("img").click(function() {  //use a class, since your ID gets mangled
-        $(".item").addClass("active");      //add the class to the clicked element
-    });
-});
